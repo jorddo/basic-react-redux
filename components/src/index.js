51dd31b0ace1de755834e6db80c7b8ed25a89ae1
jorddo <jordanbsneed@gmail.com>
2022-07-28 14:44:00 -0500

    saving new app: seasons

--- components/src/index.js
+++ components/src/index.js
@@ -3,29 +3,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import './style/App.css';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <ApprovalCard />
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4:45PM'
-        content='Hey that is pretty slick!'
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 2:00AM'
-        content='Woah dude.'
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 5:00PM'
-        content='Wow!'
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:45PM'
+          content='Hey that is pretty slick!'
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='Today at 2:00AM'
+          content='Woah dude.'
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='Yesterday at 5:00PM'
+          content='Wow!'
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
